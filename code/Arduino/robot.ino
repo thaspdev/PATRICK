05b8447f1b9d53e1b59edbbc561ec40b6e60d3f0
thaspdev <thaspdev@gmail.com>
2017-02-22 14:41:07 +0100

    Ajout du code de l'Arduino

--- code/Arduino/robot.ino
+++ code/Arduino/robot.ino
@@ -1,90 +1,167 @@
-#include <math.h>
-#include <Servo.h>
-const int pinServo = 7;
-const int moteur1[] = {4, 3};
-const double vitesse_du_son = 343; //à 20 °C, c-à-d une température ambiante
-Servo servo;
+#include <Servo.h> //On inclut la librairie Servo permettant de contrôler un servomoteur
+String commande;
+String param;
+int numberIndex;
+int motors[][2]= {{7,8}};
+int steppers[][4] = {{2,3,4,5}};
+Servo camServo;
+Servo liftServo;
+Servo dirServo;//6: caméra ; 9 direction ; 10 soulever les roues
+int del = 5;//délai en millisecondes entre les commandes données au moteur pas à pas (stepper) afin que ce dernier tourne
 void setup() {
   // put your setup code here, to run once:
-  servo.attach(pinServo);
-  int i;
-  for(i = 0; i < 2; i++){
-    pinMode(moteur1[i], OUTPUT);
+  Serial.begin(9600); //On démarre la communication en série afin de pouvoir recevoir des commandes du Raspberry Pi
+  camServo.attach(6); //On affecte un pin spécifique à chacun des servomoteurs
+  liftServo.attach(10);
+  dirServo.attach(9);
+  for (int i = 0; i < sizeof(motors)/sizeof(motors[0]); i++) {
+    for (int j = 0; j < sizeof(motors[0])/sizeof(motors[0][0]); j++) {
+      pinMode(motors[i][j], OUTPUT);
+    }
+  }
+  for (int i = 0; i < sizeof(steppers)/sizeof(steppers[0]); i++) {
+    for (int j = 0; j < sizeof(steppers[0])/sizeof(steppers[0][0]); j++) {
+      pinMode(steppers[i][j], OUTPUT);
+    }
   }
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  double a = 0;
-  double b = 0;
-  double c = 2;
-  double t1 = 0.220;
-  double t2 = 0.350;
-  double t3 = 0.360;
-  calcul_ab(t1,t2,&a,&b);
-  int beta = calcul_beta(a,b,c);
-  servo.write(0);
-  delay(3000);
-  if (beta>=0) {
-    servo.write(beta);
-    digitalWrite(moteur1[0], HIGH);
-    digitalWrite(moteur1[1], LOW);
-    delay(3000);
-  } else if (beta<0) {
-    servo.write(180+beta);
-    digitalWrite(moteur1[0], LOW);
-    digitalWrite(moteur1[1], HIGH);
-    delay(3000);
+  obtenirCommande(); //Cette fonction sert à recevoir une commande du Raspberry Pi
+  exécuterActionSuivante(); //Celle-ci execute l'action
+  photores(); //Cette fonction détermine s'il faut allumer les LEDs afin d'éclairer les visages à détecter
+}
+void obtenirCommande() {
+  commande=Serial.readString();
+}
+void exécuterActionSuivante() {
+    //Commande du type MO:01:START
+    //Index:           0123456789{10}
+    if (commande.substring(0,2) == "MO") {
+      numberIndex=commande.substring(3,5).toInt()-1;
+      param=commande.substring(6);
+      int curMotor[] = {motors[numberIndex][0],motors[numberIndex][1]};
+      if (param.substring(0,5) == "START") {
+        digitalWrite(curMotor[0],HIGH);
+        digitalWrite(curMotor[1],LOW);
+      } else if (param.substring(0,3) == "REV") {
+        digitalWrite(curMotor[0],LOW);
+        digitalWrite(curMotor[1],HIGH);
+      } else if (param.substring(0,4) == "STOP") {
+        digitalWrite(curMotor[0],LOW);
+        digitalWrite(curMotor[1],LOW);
+      }
+    } else if (commande.substring(0,2) == "SV") {
+      numberIndex=commande.substring(3,5).toInt();
+      int angle=commande.substring(6).toInt();
+      if (numberIndex == 1){
+        camServo.write(angle);
+      } else if (numberIndex == 2){
+        liftServo.write(angle);
+      } else if (numberIndex == 3) {
+        dirServo.write(angle);
+      }
+    } else if (commande.substring(0,2) == "ST") {
+      Serial.println("ST");
+      numberIndex=commande.substring(3,5).toInt()-1;
+      int angle = commande.substring(6).toInt();
+      
+      if(numberIndex<sizeof(steppers)/sizeof(steppers[0])){
+      if (angle > 0){
+        for (int i = 0; i < angle;i++) {
+          //Serial.println(i);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+        }
+      } else {
+        for (int i = 0; i > angle; i--) {
+          //Serial.println(i);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], HIGH);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], LOW); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], HIGH);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], LOW);
+          delay(del);
+          digitalWrite(steppers[numberIndex][0], HIGH); 
+          digitalWrite(steppers[numberIndex][1], LOW);
+          digitalWrite(steppers[numberIndex][2], LOW);
+          digitalWrite(steppers[numberIndex][3], HIGH);
+        }
+      }
+    }
   }
 }
-
-int calcul_beta(double a, double b, double c){
-  return cos((a*a+c*c-b*b)/2*a*c)*180/PI;
-}
-
-void calcul_ab(double temps_1er, double temps_2e, double *a, double *b){
-  *b = temps_1er*vitesse_du_son/2;
-  *a = temps_2e*vitesse_du_son/2;
-}
-
-bool cote_objet(double temps_2e, double temps_3e, bool cote_mesure) {//Si le côté de la mesure est vers la gauche, cote_mesure vaut 0, sinon 1
-  //Si l'objet est vers la gauche, cote_objet vaut 0, sinon 1
-  if(cote_mesure) {//S'il va vers la droite pour mesurer
-    if(temps_2e < temps 3e) {
-      //Alors l'objet est vers la gauche
-      return 0;
-    } else {
-      //Alors l'objet est vers la droite
-      return 1;
-    }
-  } else {
-    if(temps_2e < temps 3e) {
-      //Alors l'objet est vers la droite
-      return 1;
-    } else {
-      //Alors l'objet est vers la gauche  
-      return 0;
-    }
-  }
-}
-int angle_servo(bool cote_objet, int beta){
-    if (cote_objet) {
-      return beta;
-    } else {
-      return 180-beta;
-    }
-}
-
-void tourner_et_avancer(bool cote_objet, int angle_servo) {
-  servo.write(angle_servo);
-  if (cote_objet) {
-    tourner_moteur();  
-  }
-}
-void tourner_moteur(bool sens){
-  if(sens) {
-    
-  } else {
-    
+void photores() {
+  int lum = analogRead(0);
+  if (lum < 500) { //Nous avons déterminé que si cette valeur, pouvant aller jusqu'à 1023, était inférieure à 500, il fallait éclairer les visages à détecter
+    for (int pinLED = 1; pinLED <= 4; l++) {    
+          digitalWrite(pinLED, HIGH); //On allume les 4 LEDs
+        }
   }
 }
